43dce00007b0d08d95c5386d7a09b5ba2bdbea49
Gaz1676 <gazfleming@gmail.com>
2018-01-07 00:56:25 +0000

    Introduced retrofit to the main app

--- app/src/main/java/app/mytweetapp/main/MyTweetApp.java
+++ app/src/main/java/app/mytweetapp/main/MyTweetApp.java
@@ -1,14 +1,29 @@
 package app.mytweetapp.main;
 
-/**
- * Created by gary on 04/01/18.
- */
+
 import android.app.Application;
+import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-import static app.mytweetapp.helpers.LogHelpers.info;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.mytweetapp.models.Tweet;
+import app.mytweetapp.models.User;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MyTweetApp extends Application {
+
+    public TweetService tweetService;
+    public boolean tweetServiceAvailable = false;
+    public String service_url = "https://twitter-web.herokuapp.com/";
+
+    public static User currentUser;
+    public List<Tweet> tweets = new ArrayList<>();
+    public List<User> users = new ArrayList<>();
 
     protected static MyTweetApp app;
 
@@ -16,7 +31,42 @@ public class MyTweetApp extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
-        info(this, "MyTweetApp is launched");
+        Gson gson = new GsonBuilder().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(service_url)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        tweetService = retrofit.create(TweetService.class);
+        Log.v("Tweet", "MyTweetApp started");
+    }
+
+
+    public void newUser(User user) {
+        users.add(user);
+    }
+
+
+    public void newTweet(Tweet tweet) {
+        tweets.add(tweet);
+    }
+
+
+    // validates the user by their email & password
+    // this lets the app know that it is a current user
+    public boolean validUser(String email, String password) {
+        for (User user : users) {
+            if (user.email.equals(email) && user.password.equals(password)) {
+                currentUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static String getCurrentUser() {
+        return currentUser._id;
     }
 
     public static MyTweetApp getApp() {
